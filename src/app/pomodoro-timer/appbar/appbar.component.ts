2769b25811788d9f71ca48b78d264abb4b402e85
LFLucas <lf.ferrarilucas@gmail.com>
2025-06-06 19:20:58 -0300

    base do estilo pronta

---
+++ src/app/pomodoro-timer/appbar/appbar.component.ts
@@ -0,0 +1,10 @@
+import { Component, HostBinding, Output } from '@angular/core';
+
+@Component({
+  selector: 'pt-appbar',
+  templateUrl: './appbar.component.html',
+  styleUrl: './appbar.component.sass'
+})
+export class AppbarComponent {
+  
+}
